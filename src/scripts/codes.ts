645822dd29cb94c7479c6447e65e6eae925d474d
Akarshan <107797503+aku47z@users.noreply.github.com>
2025-02-13 23:06:11 +0545

    Form Added & SEO

---
+++ src/scripts/codes.ts
@@ -0,0 +1,962 @@
+export const codes = [
+  {
+    countryCodes: ["93"],
+    isoCode3: "AFG",
+  },
+  {
+    countryCodes: ["355"],
+    isoCode3: "ALB",
+  },
+  {
+    countryCodes: ["213"],
+    isoCode3: "DZA",
+  },
+  {
+    countryCodes: ["1-684"],
+    isoCode3: "ASM",
+  },
+  {
+    countryCodes: ["376"],
+    isoCode3: "AND",
+  },
+  {
+    countryCodes: ["244"],
+    isoCode3: "AGO",
+  },
+  {
+    countryCodes: ["1-264"],
+    isoCode3: "AIA",
+  },
+  {
+    countryCodes: ["672"],
+    isoCode3: "ATA",
+  },
+  {
+    countryCodes: ["1-268"],
+    isoCode3: "ATG",
+  },
+  {
+    countryCodes: ["54"],
+    isoCode3: "ARG",
+  },
+  {
+    countryCodes: ["374"],
+    isoCode3: "ARM",
+  },
+  {
+    countryCodes: ["297"],
+    isoCode3: "ABW",
+  },
+  {
+    countryCodes: ["61"],
+    isoCode3: "AUS",
+  },
+  {
+    countryCodes: ["43"],
+    isoCode3: "AUT",
+  },
+  {
+    countryCodes: ["994"],
+    isoCode3: "AZE",
+  },
+  {
+    countryCodes: ["1-242"],
+    isoCode3: "BHS",
+  },
+  {
+    countryCodes: ["973"],
+    isoCode3: "BHR",
+  },
+  {
+    countryCodes: ["880"],
+    isoCode3: "BGD",
+  },
+  {
+    countryCodes: ["1-246"],
+    isoCode3: "BRB",
+  },
+  {
+    countryCodes: ["375"],
+    isoCode3: "BLR",
+  },
+  {
+    countryCodes: ["32"],
+    isoCode3: "BEL",
+  },
+  {
+    countryCodes: ["501"],
+    isoCode3: "BLZ",
+  },
+  {
+    countryCodes: ["229"],
+    isoCode3: "BEN",
+  },
+  {
+    countryCodes: ["1-441"],
+    isoCode3: "BMU",
+  },
+  {
+    countryCodes: ["975"],
+    isoCode3: "BTN",
+  },
+  {
+    countryCodes: ["591"],
+    isoCode3: "BOL",
+  },
+  {
+    countryCodes: ["387"],
+    isoCode3: "BIH",
+  },
+  {
+    countryCodes: ["267"],
+    isoCode3: "BWA",
+  },
+  {
+    countryCodes: ["55"],
+    isoCode3: "BRA",
+  },
+  {
+    countryCodes: ["246"],
+    isoCode3: "IOT",
+  },
+  {
+    countryCodes: ["1-284"],
+    isoCode3: "VGB",
+  },
+  {
+    countryCodes: ["673"],
+    isoCode3: "BRN",
+  },
+  {
+    countryCodes: ["359"],
+    isoCode3: "BGR",
+  },
+  {
+    countryCodes: ["226"],
+    isoCode3: "BFA",
+  },
+  {
+    countryCodes: ["257"],
+    isoCode3: "BDI",
+  },
+  {
+    countryCodes: ["855"],
+    isoCode3: "KHM",
+  },
+  {
+    countryCodes: ["237"],
+    isoCode3: "CMR",
+  },
+  {
+    countryCodes: ["1"],
+    isoCode3: "CAN",
+  },
+  {
+    countryCodes: ["238"],
+    isoCode3: "CPV",
+  },
+  {
+    countryCodes: ["1-345"],
+    isoCode3: "CYM",
+  },
+  {
+    countryCodes: ["236"],
+    isoCode3: "CAF",
+  },
+  {
+    countryCodes: ["235"],
+    isoCode3: "TCD",
+  },
+  {
+    countryCodes: ["56"],
+    isoCode3: "CHL",
+  },
+  {
+    countryCodes: ["86"],
+    isoCode3: "CHN",
+  },
+  {
+    countryCodes: ["61"],
+    isoCode3: "CXR",
+  },
+  {
+    countryCodes: ["61"],
+    isoCode3: "CCK",
+  },
+  {
+    countryCodes: ["57"],
+    isoCode3: "COL",
+  },
+  {
+    countryCodes: ["269"],
+    isoCode3: "COM",
+  },
+  {
+    countryCodes: ["682"],
+    isoCode3: "COK",
+  },
+  {
+    countryCodes: ["506"],
+    isoCode3: "CRI",
+  },
+  {
+    countryCodes: ["385"],
+    isoCode3: "HRV",
+  },
+  {
+    countryCodes: ["53"],
+    isoCode3: "CUB",
+  },
+  {
+    countryCodes: ["599"],
+    isoCode3: "CUW",
+  },
+  {
+    countryCodes: ["357"],
+    isoCode3: "CYP",
+  },
+  {
+    countryCodes: ["420"],
+    isoCode3: "CZE",
+  },
+  {
+    countryCodes: ["243"],
+    isoCode3: "COD",
+  },
+  {
+    countryCodes: ["45"],
+    isoCode3: "DNK",
+  },
+  {
+    countryCodes: ["253"],
+    isoCode3: "DJI",
+  },
+  {
+    countryCodes: ["1-767"],
+    isoCode3: "DMA",
+  },
+  {
+    countryCodes: ["1-809", "1-829", "1-849"],
+    isoCode3: "DOM",
+  },
+  {
+    countryCodes: ["670"],
+    isoCode3: "TLS",
+  },
+  {
+    countryCodes: ["593"],
+    isoCode3: "ECU",
+  },
+  {
+    countryCodes: ["20"],
+    isoCode3: "EGY",
+  },
+  {
+    countryCodes: ["503"],
+    isoCode3: "SLV",
+  },
+  {
+    countryCodes: ["240"],
+    isoCode3: "GNQ",
+  },
+  {
+    countryCodes: ["291"],
+    isoCode3: "ERI",
+  },
+  {
+    countryCodes: ["372"],
+    isoCode3: "EST",
+  },
+  {
+    countryCodes: ["251"],
+    isoCode3: "ETH",
+  },
+  {
+    countryCodes: ["500"],
+    isoCode3: "FLK",
+  },
+  {
+    countryCodes: ["298"],
+    isoCode3: "FRO",
+  },
+  {
+    countryCodes: ["679"],
+    isoCode3: "FJI",
+  },
+  {
+    countryCodes: ["358"],
+    isoCode3: "FIN",
+  },
+  {
+    countryCodes: ["33"],
+    isoCode3: "FRA",
+  },
+  {
+    countryCodes: ["689"],
+    isoCode3: "PYF",
+  },
+  {
+    countryCodes: ["241"],
+    isoCode3: "GAB",
+  },
+  {
+    countryCodes: ["220"],
+    isoCode3: "GMB",
+  },
+  {
+    countryCodes: ["995"],
+    isoCode3: "GEO",
+  },
+  {
+    countryCodes: ["49"],
+    isoCode3: "DEU",
+  },
+  {
+    countryCodes: ["233"],
+    isoCode3: "GHA",
+  },
+  {
+    countryCodes: ["350"],
+    isoCode3: "GIB",
+  },
+  {
+    countryCodes: ["30"],
+    isoCode3: "GRC",
+  },
+  {
+    countryCodes: ["299"],
+    isoCode3: "GRL",
+  },
+  {
+    countryCodes: ["1-473"],
+    isoCode3: "GRD",
+  },
+  {
+    countryCodes: ["1-671"],
+    isoCode3: "GUM",
+  },
+  {
+    countryCodes: ["502"],
+    isoCode3: "GTM",
+  },
+  {
+    countryCodes: ["44-1481"],
+    isoCode3: "GGY",
+  },
+  {
+    countryCodes: ["224"],
+    isoCode3: "GIN",
+  },
+  {
+    countryCodes: ["245"],
+    isoCode3: "GNB",
+  },
+  {
+    countryCodes: ["592"],
+    isoCode3: "GUY",
+  },
+  {
+    countryCodes: ["509"],
+    isoCode3: "HTI",
+  },
+  {
+    countryCodes: ["504"],
+    isoCode3: "HND",
+  },
+  {
+    countryCodes: ["852"],
+    isoCode3: "HKG",
+  },
+  {
+    countryCodes: ["36"],
+    isoCode3: "HUN",
+  },
+  {
+    countryCodes: ["354"],
+    isoCode3: "ISL",
+  },
+  {
+    countryCodes: ["91"],
+    isoCode3: "IND",
+  },
+  {
+    countryCodes: ["62"],
+    isoCode3: "IDN",
+  },
+  {
+    countryCodes: ["98"],
+    isoCode3: "IRN",
+  },
+  {
+    countryCodes: ["964"],
+    isoCode3: "IRQ",
+  },
+  {
+    countryCodes: ["353"],
+    isoCode3: "IRL",
+  },
+  {
+    countryCodes: ["44-1624"],
+    isoCode3: "IMN",
+  },
+  {
+    countryCodes: ["972"],
+    isoCode3: "ISR",
+  },
+  {
+    countryCodes: ["39"],
+    isoCode3: "ITA",
+  },
+  {
+    countryCodes: ["225"],
+    isoCode3: "CIV",
+  },
+  {
+    countryCodes: ["1-876"],
+    isoCode3: "JAM",
+  },
+  {
+    countryCodes: ["81"],
+    isoCode3: "JPN",
+  },
+  {
+    countryCodes: ["44-1534"],
+    isoCode3: "JEY",
+  },
+  {
+    countryCodes: ["962"],
+    isoCode3: "JOR",
+  },
+  {
+    countryCodes: ["7"],
+    isoCode3: "KAZ",
+  },
+  {
+    countryCodes: ["254"],
+    isoCode3: "KEN",
+  },
+  {
+    countryCodes: ["686"],
+    isoCode3: "KIR",
+  },
+  {
+    countryCodes: ["383"],
+    isoCode3: "XKX",
+  },
+  {
+    countryCodes: ["965"],
+    isoCode3: "KWT",
+  },
+  {
+    countryCodes: ["996"],
+    isoCode3: "KGZ",
+  },
+  {
+    countryCodes: ["856"],
+    isoCode3: "LAO",
+  },
+  {
+    countryCodes: ["371"],
+    isoCode3: "LVA",
+  },
+  {
+    countryCodes: ["961"],
+    isoCode3: "LBN",
+  },
+  {
+    countryCodes: ["266"],
+    isoCode3: "LSO",
+  },
+  {
+    countryCodes: ["231"],
+    isoCode3: "LBR",
+  },
+  {
+    countryCodes: ["218"],
+    isoCode3: "LBY",
+  },
+  {
+    countryCodes: ["423"],
+    isoCode3: "LIE",
+  },
+  {
+    countryCodes: ["370"],
+    isoCode3: "LTU",
+  },
+  {
+    countryCodes: ["352"],
+    isoCode3: "LUX",
+  },
+  {
+    countryCodes: ["853"],
+    isoCode3: "MAC",
+  },
+  {
+    countryCodes: ["389"],
+    isoCode3: "MKD",
+  },
+  {
+    countryCodes: ["261"],
+    isoCode3: "MDG",
+  },
+  {
+    countryCodes: ["265"],
+    isoCode3: "MWI",
+  },
+  {
+    countryCodes: ["60"],
+    isoCode3: "MYS",
+  },
+  {
+    countryCodes: ["960"],
+    isoCode3: "MDV",
+  },
+  {
+    countryCodes: ["223"],
+    isoCode3: "MLI",
+  },
+  {
+    countryCodes: ["356"],
+    isoCode3: "MLT",
+  },
+  {
+    countryCodes: ["692"],
+    isoCode3: "MHL",
+  },
+  {
+    countryCodes: ["222"],
+    isoCode3: "MRT",
+  },
+  {
+    countryCodes: ["230"],
+    isoCode3: "MUS",
+  },
+  {
+    countryCodes: ["262"],
+    isoCode3: "MYT",
+  },
+  {
+    countryCodes: ["52"],
+    isoCode3: "MEX",
+  },
+  {
+    countryCodes: ["691"],
+    isoCode3: "FSM",
+  },
+  {
+    countryCodes: ["373"],
+    isoCode3: "MDA",
+  },
+  {
+    countryCodes: ["377"],
+    isoCode3: "MCO",
+  },
+  {
+    countryCodes: ["976"],
+    isoCode3: "MNG",
+  },
+  {
+    countryCodes: ["382"],
+    isoCode3: "MNE",
+  },
+  {
+    countryCodes: ["1-664"],
+    isoCode3: "MSR",
+  },
+  {
+    countryCodes: ["212"],
+    isoCode3: "MAR",
+  },
+  {
+    countryCodes: ["258"],
+    isoCode3: "MOZ",
+  },
+  {
+    countryCodes: ["95"],
+    isoCode3: "MMR",
+  },
+  {
+    countryCodes: ["264"],
+    isoCode3: "NAM",
+  },
+  {
+    countryCodes: ["674"],
+    isoCode3: "NRU",
+  },
+  {
+    countryCodes: ["977"],
+    isoCode3: "NPL",
+  },
+  {
+    countryCodes: ["31"],
+    isoCode3: "NLD",
+  },
+  {
+    countryCodes: ["599"],
+    isoCode3: "ANT",
+  },
+  {
+    countryCodes: ["687"],
+    isoCode3: "NCL",
+  },
+  {
+    countryCodes: ["64"],
+    isoCode3: "NZL",
+  },
+  {
+    countryCodes: ["505"],
+    isoCode3: "NIC",
+  },
+  {
+    countryCodes: ["227"],
+    isoCode3: "NER",
+  },
+  {
+    countryCodes: ["234"],
+    isoCode3: "NGA",
+  },
+  {
+    countryCodes: ["683"],
+    isoCode3: "NIU",
+  },
+  {
+    countryCodes: ["850"],
+    isoCode3: "PRK",
+  },
+  {
+    countryCodes: ["1-670"],
+    isoCode3: "MNP",
+  },
+  {
+    countryCodes: ["47"],
+    isoCode3: "NOR",
+  },
+  {
+    countryCodes: ["968"],
+    isoCode3: "OMN",
+  },
+  {
+    countryCodes: ["92"],
+    isoCode3: "PAK",
+  },
+  {
+    countryCodes: ["680"],
+    isoCode3: "PLW",
+  },
+  {
+    countryCodes: ["970"],
+    isoCode3: "PSE",
+  },
+  {
+    countryCodes: ["507"],
+    isoCode3: "PAN",
+  },
+  {
+    countryCodes: ["675"],
+    isoCode3: "PNG",
+  },
+  {
+    countryCodes: ["595"],
+    isoCode3: "PRY",
+  },
+  {
+    countryCodes: ["51"],
+    isoCode3: "PER",
+  },
+  {
+    countryCodes: ["63"],
+    isoCode3: "PHL",
+  },
+  {
+    countryCodes: ["64"],
+    isoCode3: "PCN",
+  },
+  {
+    countryCodes: ["48"],
+    isoCode3: "POL",
+  },
+  {
+    countryCodes: ["351"],
+    isoCode3: "PRT",
+  },
+  {
+    countryCodes: ["1-787", "1-939"],
+    isoCode3: "PRI",
+  },
+  {
+    countryCodes: ["974"],
+    isoCode3: "QAT",
+  },
+  {
+    countryCodes: ["242"],
+    isoCode3: "COG",
+  },
+  {
+    countryCodes: ["262"],
+    isoCode3: "REU",
+  },
+  {
+    countryCodes: ["40"],
+    isoCode3: "ROU",
+  },
+  {
+    countryCodes: ["7"],
+    isoCode3: "RUS",
+  },
+  {
+    countryCodes: ["250"],
+    isoCode3: "RWA",
+  },
+  {
+    countryCodes: ["590"],
+    isoCode3: "BLM",
+  },
+  {
+    countryCodes: ["290"],
+    isoCode3: "SHN",
+  },
+  {
+    countryCodes: ["1-869"],
+    isoCode3: "KNA",
+  },
+  {
+    countryCodes: ["1-758"],
+    isoCode3: "LCA",
+  },
+  {
+    countryCodes: ["590"],
+    isoCode3: "MAF",
+  },
+  {
+    countryCodes: ["508"],
+    isoCode3: "SPM",
+  },
+  {
+    countryCodes: ["1-784"],
+    isoCode3: "VCT",
+  },
+  {
+    countryCodes: ["685"],
+    isoCode3: "WSM",
+  },
+  {
+    countryCodes: ["378"],
+    isoCode3: "SMR",
+  },
+  {
+    countryCodes: ["239"],
+    isoCode3: "STP",
+  },
+  {
+    countryCodes: ["966"],
+    isoCode3: "SAU",
+  },
+  {
+    countryCodes: ["221"],
+    isoCode3: "SEN",
+  },
+  {
+    countryCodes: ["381"],
+    isoCode3: "SRB",
+  },
+  {
+    countryCodes: ["248"],
+    isoCode3: "SYC",
+  },
+  {
+    countryCodes: ["232"],
+    isoCode3: "SLE",
+  },
+  {
+    countryCodes: ["65"],
+    isoCode3: "SGP",
+  },
+  {
+    countryCodes: ["1-721"],
+    isoCode3: "SXM",
+  },
+  {
+    countryCodes: ["421"],
+    isoCode3: "SVK",
+  },
+  {
+    countryCodes: ["386"],
+    isoCode3: "SVN",
+  },
+  {
+    countryCodes: ["677"],
+    isoCode3: "SLB",
+  },
+  {
+    countryCodes: ["252"],
+    isoCode3: "SOM",
+  },
+  {
+    countryCodes: ["27"],
+    isoCode3: "ZAF",
+  },
+  {
+    countryCodes: ["82"],
+    isoCode3: "KOR",
+  },
+  {
+    countryCodes: ["211"],
+    isoCode3: "SSD",
+  },
+  {
+    countryCodes: ["34"],
+    isoCode3: "ESP",
+  },
+  {
+    countryCodes: ["94"],
+    isoCode3: "LKA",
+  },
+  {
+    countryCodes: ["249"],
+    isoCode3: "SDN",
+  },
+  {
+    countryCodes: ["597"],
+    isoCode3: "SUR",
+  },
+  {
+    countryCodes: ["47"],
+    isoCode3: "SJM",
+  },
+  {
+    countryCodes: ["268"],
+    isoCode3: "SWZ",
+  },
+  {
+    countryCodes: ["46"],
+    isoCode3: "SWE",
+  },
+  {
+    countryCodes: ["41"],
+    isoCode3: "CHE",
+  },
+  {
+    countryCodes: ["963"],
+    isoCode3: "SYR",
+  },
+  {
+    countryCodes: ["886"],
+    isoCode3: "TWN",
+  },
+  {
+    countryCodes: ["992"],
+    isoCode3: "TJK",
+  },
+  {
+    countryCodes: ["255"],
+    isoCode3: "TZA",
+  },
+  {
+    countryCodes: ["66"],
+    isoCode3: "THA",
+  },
+  {
+    countryCodes: ["228"],
+    isoCode3: "TGO",
+  },
+  {
+    countryCodes: ["690"],
+    isoCode3: "TKL",
+  },
+  {
+    countryCodes: ["676"],
+    isoCode3: "TON",
+  },
+  {
+    countryCodes: ["1-868"],
+    isoCode3: "TTO",
+  },
+  {
+    countryCodes: ["216"],
+    isoCode3: "TUN",
+  },
+  {
+    countryCodes: ["90"],
+    isoCode3: "TUR",
+  },
+  {
+    countryCodes: ["993"],
+    isoCode3: "TKM",
+  },
+  {
+    countryCodes: ["1-649"],
+    isoCode3: "TCA",
+  },
+  {
+    countryCodes: ["688"],
+    isoCode3: "TUV",
+  },
+  {
+    countryCodes: ["1-340"],
+    isoCode3: "VIR",
+  },
+  {
+    countryCodes: ["256"],
+    isoCode3: "UGA",
+  },
+  {
+    countryCodes: ["380"],
+    isoCode3: "UKR",
+  },
+  {
+    countryCodes: ["971"],
+    isoCode3: "ARE",
+  },
+  {
+    countryCodes: ["44"],
+    isoCode3: "GBR",
+  },
+  {
+    countryCodes: ["1"],
+    isoCode3: "USA",
+  },
+  {
+    countryCodes: ["598"],
+    isoCode3: "URY",
+  },
+  {
+    countryCodes: ["998"],
+    isoCode3: "UZB",
+  },
+  {
+    countryCodes: ["678"],
+    isoCode3: "VUT",
+  },
+  {
+    countryCodes: ["379"],
+    isoCode3: "VAT",
+  },
+  {
+    countryCodes: ["58"],
+    isoCode3: "VEN",
+  },
+  {
+    countryCodes: ["84"],
+    isoCode3: "VNM",
+  },
+  {
+    countryCodes: ["681"],
+    isoCode3: "WLF",
+  },
+  {
+    countryCodes: ["212"],
+    isoCode3: "ESH",
+  },
+  {
+    countryCodes: ["967"],
+    isoCode3: "YEM",
+  },
+  {
+    countryCodes: ["260"],
+    isoCode3: "ZMB",
+  },
+  {
+    countryCodes: ["263"],
+    isoCode3: "ZWE",
+  },
+];
